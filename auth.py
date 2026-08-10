c3c6e3c711ea5fa0c74af00867ea8dc384710e86
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-08-10 19:37:59 +0530

    add get current user and its dependency (alias)

--- auth.py
+++ auth.py
@@ -6,6 +6,16 @@ from pwdlib import PasswordHash
 
 from config import settings
 
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import models
+from config import settings
+from database import get_db
+
 # Create a secure password hashing configuration using the recommended algorithm
 password_hash = PasswordHash.recommended()
 
@@ -69,4 +79,44 @@ def verify_access_token(token: str) -> str | None:
     else:
         # Return the user ID stored in the "sub" field
         return payload.get("sub")
+
+# Get Current User
+async def get_current_user(
+        token: Annotated[str, Depends(oauth2_scheme)],
+        db: Annotated[AsyncSession, Depends(get_db)]) -> models.User:
+
+    user_id = verify_access_token(token)
+
+    if user_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+            headers={"WWW-Authenticated": "Bearer"}
+        )    
+
+    try:
+        user_id_int = int(user_id)
+    except (TypeError, ValueError):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token!",
+            headers={"WWW-Authenticated": "Bearer"}
+        )
+
+    result = await db.execute(
+        select(models.User).where(models.User.id == user_id_int)
+    )
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found!",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+    return user
+
+# Alias for injecting the authenticated/current user into route handlers
+CurrentUser = Annotated[models.User, Depends(get_current_user)]
 
