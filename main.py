7657f6cf9302f2e2d2d2bb4423dd89f4c6608062
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-08-20 17:08:48 +0530

    add pagination functionality

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from starlette.exceptions import HTTPException as StarletteHTTPException
 from typing import Annotated
 from contextlib import asynccontextmanager
 
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
@@ -16,6 +16,8 @@ import models
 from database import Base, engine, get_db
 
 from routers import users, posts
+
+from config import settings
 
 # Lifespan Function
 @asynccontextmanager
@@ -45,16 +47,30 @@ app.include_router(posts.router, prefix="/api/posts", tags=["posts"])
 @app.get("/", include_in_schema=False, name="home")   # this routes will not appear in /docs. Keeps html and api routes seperate.
 @app.get("/posts", include_in_schema=False, name="posts") 
 async def home(request: Request, db: Annotated[AsyncSession, Depends(get_db)]):
+    count_result = await db.execute(select(func.count()).select_from(models.Post))
+    total = count_result.scalar() or 0
+
     result = await db.execute(
         select(models.Post)
         .options(selectinload(models.Post.author))
         .order_by(models.Post.date_posted.desc())
+        .limit(settings.posts_per_page)
     )
-    posts = result.scalars().all() # returns all posts
+
+    posts = result.scalars().all()   # returns all posts
+
+    # Check if there are more posts available to fetch (True or False)
+    has_more = len(posts) < total
+
     return templates.TemplateResponse(
         request, 
         "home.html", 
-        {"posts": posts, "title": "Home"},
+        {
+            "posts": posts, 
+            "title": "Home",
+            "limit": settings.posts_per_page,
+            "has_more": has_more
+        }
     )
 
 # Post Page
@@ -88,15 +104,36 @@ async def user_posts_page(request: Request, user_id: int, db: Annotated[AsyncSes
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found!")
 
+    count_result = await db.execute(
+        select(func.count())
+        .select_from(models.Post)
+        .where(models.Post.user_id == user_id)
+    )
+
+    total = count_result.scalar() or 0
+    
     result = await db.execute(
         select(models.Post)
         .options(selectinload(models.Post.author))
         .where(models.Post.user_id == user_id)
         .order_by(models.Post.date_posted.desc())
+        .limit(settings.posts_per_page)
     )
     posts = result.scalars().all()
 
-    return templates.TemplateResponse(request, "user_posts.html", {"posts": posts, "user": user, "title": f"{user.username}'s Posts"})
+    has_more = len(posts) < total
+
+    return templates.TemplateResponse(
+        request, 
+        "user_posts.html", 
+        {
+            "posts": posts, 
+            "user": user, 
+            "title": f"{user.username}'s Posts",
+            "limit": settings.posts_per_page,
+            "has_more": has_more
+        }
+    )
 
 # Login Template Route
 @app.get("/login", include_in_schema=False)
