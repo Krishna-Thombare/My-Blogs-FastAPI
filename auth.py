b59e214784b18b7a575366fb1fa86b94c0c09717
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-08-23 13:16:10 +0530

    add reset token

--- auth.py
+++ auth.py
@@ -15,6 +15,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 import models
 from database import get_db
 
+import hashlib
+import secrets
+
 # Create a secure password hashing configuration using the recommended algorithm
 password_hash = PasswordHash.recommended()
 
@@ -29,6 +32,12 @@ def hash_password(password: str) -> str:
 # Verify a plain-text password against the hashed password stored in the database
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_hash.verify(plain_password, hashed_password)
+
+def generate_reset_token() -> str:
+    return secrets.token_urlsafe(32)
+
+def hash_reset_token(token: str) -> str:
+    return hashlib.sha256(token.encode()).hexdigest()
 
 # Create Access Token
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
