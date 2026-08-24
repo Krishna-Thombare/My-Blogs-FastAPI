6daa3ccca31c3110ec923bfe311e8e0eea3af4f7
Krishna-Thombare <Krishnathombare43@gmail.com>
2026-08-24 19:02:22 +0530

    add forgot & reset password routes

--- main.py
+++ main.py
@@ -162,6 +162,19 @@ async def account_page(request: Request):
         {"title": "Account"}
     )
 
+# Forgot Password Route
+@app.get("/forgot-password", include_in_schema=False)
+async def forgot_password_page(request: Request):
+    return templates.TemplateResponse(request, "forgot_password.html", {"title": "Forgot password"})
+
+# Reset Password Route
+@app.get("/reset-password", include_in_schema=False)
+async def reset_password_page(request: Request):
+    response = templates.TemplateResponse(request, "reset_password.html", {"title": "Reset Password"})
+    response.headers["Referrer-Policy"] = "no-referrer"
+
+    return response
+
 # ------- Exception Handlers ------------------------------------------------------------------------------------------------------------------------------
 @app.exception_handler(StarletteHTTPException)
 async def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
